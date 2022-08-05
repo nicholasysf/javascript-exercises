af8d0820a74a74815ef1d1324394c6bacc344332
Nicholasysf <nicholasysf@gmail.com>
2022-08-06 02:41:16 +0800

    Finished exercise 2 .

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,19 @@
-const repeatString = function() {
-
+const repeatString = function(string,num) {
+    //Initialize an empty string first
+    let repeatedString = "";
+    //A loop that would return error if the give number is lesser than 0
+    while(num<0){
+        return "ERROR";
+    }
+    //A loop that would repeat the string
+    while(num>0){
+        repeatedString += string;
+        num--;
+    }
+    return repeatedString;
 };
 
 // Do not edit below this line
 module.exports = repeatString;
+
+
