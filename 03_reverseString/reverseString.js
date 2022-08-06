9a6b2964ab67c7d9147807490040f3f934e9ce87
Nicholasysf <nicholasysf@gmail.com>
2022-08-07 02:36:45 +0800

    Finished exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,9 @@
-const reverseString = function() {
-
-};
+const reverseString = function(string) {
+    let splitString = string.split("");
+    let reverseString = splitString.reverse();
+    let joinString = reverseString.join("");
+    return joinString;
+}
 
 // Do not edit below this line
 module.exports = reverseString;
