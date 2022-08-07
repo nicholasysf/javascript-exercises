67bd1cac22185c0cd21784838ff253c5fc93e47f
Nicholasysf <nicholasysf@gmail.com>
2022-08-07 23:28:35 +0800

    Semi-done with this question, but still need to fully revisit again.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
+    //This gives us the given array for eg : [1,2,3,4,5]
+    const givenArray = args[0];
+    return givenArray = givenArray.filter(num =>!args.includes(num))
 
 };
 
-// Do not edit below this line
+// Do not edit below this line›
 module.exports = removeFromArray;
