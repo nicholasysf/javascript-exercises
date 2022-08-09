3d5981f5ac12c213d34cac9814ec1425d70ff453
Nicholasysf <nicholasysf@gmail.com>
2022-08-09 18:00:56 +0800

    Finished exercise 07

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,29 @@
-const sumAll = function() {
 
-};
+const sumAll = function(num1,num2) {
+
+    let sum = 0;
+    
+    if(typeof num1 !="number"|| typeof num2 !="number"){
+        return "ERROR";
+
+    }else if(num1 < 0 || num2 < 0){
+        return "ERROR";
+
+    }else if(num1>num2){
+        for(i=num2; i<=num1; i++) {
+            sum = sum + num2;
+            num2 ++;
+        }
+        return sum;
+        
+    }else if(num2>num1){
+        for(i=num1; i<=num2; i++){
+            sum = sum + num1;
+            num1++;
+        }
+        return sum;
+    } 
+}
 
 // Do not edit below this line
 module.exports = sumAll;
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,14 @@
-const ftoc = function() {
+//This function converts farenheit to Celsius.
+//The formula to convert f to c is -32 and x 5/9
 
+const ftoc = function(f) {
+  return Math.round( (f-32) * (5/9) * 10)/10;
 };
 
-const ctof = function() {
-
+//This function converts Celsius to Farenheit
+//The formula to convert C to F is multiply 1.8 and add 32
+const ctof = function(c) {
+  return Math.round( ((c*1.8) + 32) * 10)/10;
 };
 
 // Do not edit below this line
