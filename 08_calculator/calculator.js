ff2bc1abc955675281ff991129cda715becbb404
Nicholasysf <nicholasysf@gmail.com>
2022-08-18 22:12:10 +0800

    Finished exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
-	
+const add = function(a,b) {
+	let sum = a+b;
+  return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	let sub = a-b;
+  return sub;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let total = 0;
+  for(let i = 0; i < arr.length ; i++){
+    total += arr[i];}
+  return total;
+
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let multiplied = array.reduce((accumulator,currentValue) => {
+    return accumulator * currentValue;
+});
+  return multiplied;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	let poweredUp = a ** b ;
+  return poweredUp;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  let result = 1;
+	if(n==0 || n==1 ){
+    return 1;
+  }
+  while (n>1){
+    result *= n
+    n--;
+  }
+  return result;
 };
 
 // Do not edit below this line
