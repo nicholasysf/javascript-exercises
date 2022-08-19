8a5ee718b0022b009a32db8417d4fd9f31c1ae75
Nicholasysf <nicholasysf@gmail.com>
2022-08-19 17:53:17 +0800

    FInished finding the oldest exercises using array reduce method

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,24 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people.reduce((oldest,current)=>{
+        const oldestAge = getAge(oldest.yearOfBirth,oldest.yearOfDeath);
+        const currentAge = getAge(current.yearOfBirth,current.yearOfDeath);
+        if(currentAge>oldestAge){
+            return current;
+        }else{
+            return oldest;
+        }
+    })
 
 };
 
+
+//Get the age function
+
+function getAge(birth,death){
+    if(!death){
+        death = new Date().getFullYear();
+    }
+    return death - birth;
+}
 // Do not edit below this line
 module.exports = findTheOldest;
